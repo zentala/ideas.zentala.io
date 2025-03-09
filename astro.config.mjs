61e41e69ca03acc6976714dbbfb26c3b953931ec
Paweł Żentała <zentala@gmail.com>
2025-03-09 10:46:05 +0100

    Update domain to ideas.zentala.io and fix GitHub Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://ideas.zentala.agency',
+  site: 'https://ideas.zentala.io',
   integrations: [
     mdx(),
     sitemap(),
